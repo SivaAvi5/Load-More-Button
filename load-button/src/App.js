325192b764a23e67c20f7ccb03027280bfda5690
SivaAvi5 <avissiva229@gmail.com>
2024-02-25 03:03:47 +0530

    final:load more images completed via using filter method

---
+++ load-button/src/App.js
@@ -0,0 +1,13 @@
+
+import LoadMoreButton from './LoadMoreButton';
+
+function App() {
+
+  return (
+    <div className="App">
+      <LoadMoreButton />
+    </div>
+  );
+}
+
+export default App;
---
+++ load-button/src/LoadMoreButton.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react'
+import './style.css'
+
+const LoadMoreButton = () => {
+
+    const URL = 'https://picsum.photos/v2/list?page=1&limit=50'
+
+    const [images,setImages] = useState([])
+    const [count,setCount] = useState(1)
+
+    useEffect(() =>{
+        const fetchImages = async(getURL) =>{
+            try{
+                const response = await fetch(getURL)
+                const data = await response.json()
+                setImages(data)
+            }catch(e){
+                console.log(e)
+            }
+        }
+        fetchImages(URL)
+    },[])
+console.log(count*20 >= images.length)
+console.log(count)
+
+
+
+
+    console.log(count)
+  return (
+    <div>
+        <h2>Image Gallery</h2>
+        <div className='container'>
+        {
+            images.filter((_,index) => (count === 1? index <= 20: index <= count*20)).map((filterImageItems) =>(
+
+                <img
+                key={filterImageItems.id}
+                alt= {filterImageItems.download_url}
+                src={filterImageItems.download_url}
+                style={{
+                    height:'300px',
+                    width:'400px',
+                }}
+                />
+                
+            ))
+        }
+        </div>
+        <div className='load-btn'>
+            {
+    
+          <button onClick={() => setCount(count+1)}
+            disabled={count*20 >= images.length}
+          >Load More</button>
+            }
+        </div>
+
+    </div>
+  )
+}
+
+export default LoadMoreButton
